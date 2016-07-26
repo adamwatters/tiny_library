117462d78b23861996e5bfb35901bd26d3b8d54b
Adam <adamwatters11@gmail.com>
2016-07-26 13:49:42 -0400

    added home page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,22 @@
 require 'test_helper'
 
+def setup
+  @base_title = "Tiny Library"
+  @home_title = "Home | #{@base_title}"
+end
+
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  test "should get root" do
+    get root_url
+    assert_response :success
+    assert_select "title", @home_title
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-  end
-
-  test "should get help" do
-    get static_pages_help_url
-    assert_response :success
+    assert_select "title", @home_title
   end
 
 end
