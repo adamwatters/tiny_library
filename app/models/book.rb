8911e65d0738ed03421c696554ab1ccf94a8a610
Adam <adamwatters11@gmail.com>
2016-07-28 19:36:38 -0400

    any numerical string works for isbn

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
   validates :title, presence: true
   validates :author, presence: true
-  validates :isbn, presence: true, length: {is: 10}, uniqueness:true, numericality: { only_integer: true }
+  validates :isbn, presence: true, length: {is: 10}, uniqueness:true, numericality: true
 end
