ee0b133e23390c7b81fd68ea833b05ee23bff8d1
Adam <adamwatters11@gmail.com>
2016-07-28 15:36:48 -0400

    copied needed scaffolding from rails  tutorial

---
+++ db/migrate/20160728192944_create_books.rb
@@ -0,0 +1,12 @@
+class CreateBooks < ActiveRecord::Migration[5.0]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.integer :isbn
+      t.boolean :available
+
+      t.timestamps
+    end
+  end
+end
