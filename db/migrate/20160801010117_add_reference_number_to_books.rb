58f4d9dc0b368706eed0df97e685be1bbf286b8f
Adam <adamwatters11@gmail.com>
2016-08-28 10:00:32 -0400

    edited old migration

--- db/migrate/20160801010117_add_reference_number_to_books.rb
+++ db/migrate/20160801010117_add_reference_number_to_books.rb
@@ -1,5 +1,5 @@
 class AddReferenceNumberToBooks < ActiveRecord::Migration[5.0]
   def change
-    add_column :books, :number, :string
+    add_column :books, :number, :decimal
   end
 end
--- db/migrate/20160828132938_change_number_from_string_to_decimal.rb
+++
@@ -1,9 +0,0 @@
-class ChangeNumberFromStringToDecimal < ActiveRecord::Migration[5.0]
-  def up
-    change_column :books, :number, :decimal
-  end
-
-  def down
-    change_column :books, :number, :string
-  end
-end
