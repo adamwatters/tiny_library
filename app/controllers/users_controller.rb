1501e76e600d1d8bbfcfc7b26afcdccba4d89954
Adam <adamwatters11@gmail.com>
2016-09-19 16:15:40 -0400

    displayed create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only: :destory
+  before_action :admin_user, only: [:destory, :create]
 
   def index
     @users = User.paginate(page:params[:page])
